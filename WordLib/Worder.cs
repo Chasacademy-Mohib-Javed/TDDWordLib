5081e0fb021720e5c7f497908ad8a50baeaeee49
Chasacademy-Mohib-Javed <113864047+Chasacademy-Mohib-Javed@users.noreply.github.com>
2023-08-24 14:56:10 +0200

    first commit

---
+++ WordLib/Worder.cs
@@ -0,0 +1,41 @@
+﻿namespace WordLib;
+public class Worder
+{
+    public string IsPalindromeWordChecker(string inputWord)
+    {
+
+        
+        if (string.IsNullOrEmpty(inputWord))
+        {
+            throw new ArgumentException("Input string cannot be empty. Please try again");
+        }
+
+        string inputString, reverseString = string.Empty;
+        inputString = inputWord;
+
+
+      
+        for (int i = inputWord.Length - 1; i >= 0; i--)
+        {
+            reverseString += inputWord[i];
+        }
+
+        return reverseString;
+
+
+
+    }
+
+    public int CheckingStringLength(string inputWord)
+    {
+
+        if (string.IsNullOrEmpty(inputWord))
+        {
+            throw new ArgumentException("Input string cannot be empty. Please try again");
+        }
+
+        int numbersInLetter = inputWord.Length;
+
+        return numbersInLetter;
+    }
+}
---
+++ WordLibTests/UnitTest1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using WordLib;
+namespace WordLibTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void IsPalindrome()
+    {
+        // Create an instance to test:
+        Worder worder = new Worder();
+
+        // expected output
+        string expectedResult = "radar";
+        string inputWord = "radar";
+        // Run the method under test:
+        string actualResult = worder.IsPalindromeWordChecker(inputWord);
+
+        // Assert / Verify the result:
+        Assert.AreEqual(expectedResult, actualResult);
+
+
+    }
+
+    [TestMethod]
+
+    public void IsPalindromeThrowsException()
+    {
+        
+        Worder worder = new Worder();
+
+        // expected output
+        string inputWord = null;
+
+        // Assert / Verify the result:
+        Assert.ThrowsException<ArgumentException>(() => worder.IsPalindromeWordChecker(inputWord));
+
+
+    }
+
+    [TestMethod]
+
+    public void StringLenghtCheck()
+    {
+        // Create an instance to test:
+        Worder worder = new Worder();
+
+        // expected output
+        int expectedOutput = 6;
+        string inputWord = "London";
+
+        // Run the method under test:
+        int actualResult = worder.CheckingStringLength(inputWord);
+        // Assert / Verify the result:
+        Assert.AreEqual(expectedOutput, actualResult);
+
+
+    }
+
+    [TestMethod]
+
+    public void StringLenghtCheckThrowsException()
+    {
+        // Create an instance to test:
+        Worder worder = new Worder();
+
+        // expected output
+        string inputWord = null;
+
+        // Assert / Verify the result:
+        Assert.ThrowsException<ArgumentException>(() => worder.CheckingStringLength(inputWord));
+
+
+    }
+
+
+
+
+
+}
+
+
